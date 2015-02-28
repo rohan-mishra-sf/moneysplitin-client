d890d0e49b054b87926ffbdd931c5b1539e0dbf3
Rohan Raj Mishra <rohan.mishra@sourcefuse.com>
2015-02-28 12:31:50 +0530

    test fb code

--- js/app.js
+++ js/app.js
@@ -9,3 +9,47 @@ angular.module('ngdemo', ['ngdemo.filters', 'ngdemo.services', 'ngdemo.directive
         $routeProvider.when('/user-creation', {templateUrl: 'partials/user-creation.html', controller: 'UserCreationCtrl'});
         $routeProvider.otherwise({redirectTo: '/dummy'});
     }]);
+
+
+angular.module('ngdemo', ['ngFacebook'])
+.config(['$facebookProvider', function($facebookProvider) {
+         $facebookProvider.setAppId('1592690444299818').setPermissions(['email','user_friends']);
+         }])
+.run(['$rootScope', '$window', function($rootScope, $window) {
+      (function(d, s, id) {
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) return;
+       js = d.createElement(s); js.id = id;
+       js.src = "http://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+      $rootScope.$on('fb.load', function() {
+                     $window.dispatchEvent(new Event('fb.load'));
+                     });
+      }])
+.controller('myCtrl', ['$scope', '$facebook', function($scope, $facebook) {
+                       $scope.$on('fb.auth.authResponseChange', function() {
+                                  $scope.status = $facebook.isConnected();
+                                  if($scope.status) {
+                                  $facebook.api('/me').then(function(user) {
+                                                            $scope.user = user;
+                                                            });
+                                  }
+                                  });
+                       
+                       $scope.loginToggle = function() {
+                       if($scope.status) {
+                       $facebook.logout();
+                       } else {
+                       $facebook.login();
+                       }
+                       };
+                       
+                       $scope.getFriends = function() {
+                       if(!$scope.status) return;
+                       $facebook.cachedApi('/me/friends').then(function(friends) {
+                                                               $scope.friends = friends.data;
+                                                               });
+                       }
+                       }])
+;
